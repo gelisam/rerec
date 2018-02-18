21b5c1dc6b3eb12eb8b8a8626477e1050a33e2c9
Samuel Gélineau <gelisam+github@gmail.com>
2018-02-18 18:06:17 -0500

    a variant of System.Process with an Async API
    In particular, we can use 'wait' and 'cancel' on the Async, and it
    will wait or kill the external process.

---
+++ src/System/Process/Async.hs
@@ -0,0 +1,25 @@
+module System.Process.Async where
+
+import Control.Concurrent.Async
+import Control.Exception
+import System.Process.Typed
+
+
+execute :: FilePath -> [String] -> IO (Async ())
+execute cmd args = do
+  let processConfig = setStdin closed
+                    . setStdout closed
+                    . setStderr closed
+                    $ proc cmd args
+  process <- startProcess processConfig
+  async $ do
+    r <- try $ checkExitCode process
+    case r :: Either SomeException () of
+      Right () -> pure ()
+      Left e -> do
+        stopProcess process
+
+execute_ :: FilePath -> [String] -> IO ()
+execute_ cmd args = do
+  thread <- execute cmd args
+  wait thread
