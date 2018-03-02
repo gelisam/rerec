8d42ea22a0074a4083938299dcd39f9a880037a4
Samuel Gélineau <gelisam+github@gmail.com>
2018-03-02 07:42:44 -0500

    move interplay's implementation to the library
    so it can be called easily from other programs, like doctest

--- interplay/app/main.hs
+++ interplay/app/main.hs
@@ -1,41 +1,8 @@
-{-# LANGUAGE ViewPatterns #-}
 module Main where
 
-import System.Directory
 import System.Environment
-import System.Exit
-import Text.Printf
-import Text.Read
 
-import Lib.Interplay
-
-
-parseAudioVector :: String -> IO (Maybe AudioVector)
-parseAudioVector (readMaybe -> Just delay) = pure . Just . silence $ delay
-parseAudioVector filePath = do
-  fileExists <- doesFileExist filePath
-  if fileExists
-  then Just <$> load filePath
-  else pure $ Nothing
-
-
-printUsage :: IO ()
-printUsage = do
-  progName <- getProgName
-  putStrLn "usage:"
-  printf   "  %s [FILE | DELAY]\n" progName
-  putStrLn ""
-  putStrLn "Plays each of the audio FILEs one after the other."
-  putStrLn "DELAY pauses for the given number of seconds."
-  putStrLn "Use negative delays to overlay the end of one FILE on the beginning of the next!"
+import Exe.Interplay
 
 main :: IO ()
-main = do
-  args <- getArgs
-  audioVectorMay <- mconcat <$> traverse parseAudioVector args
-  case audioVectorMay of
-    Nothing -> do
-      printUsage
-      exitFailure
-    Just audioVector -> do
-      play_ audioVector
+main = interplay =<< getArgs
---
+++ interplay/src/Exe/Interplay.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE ViewPatterns #-}
+module Exe.Interplay where
+
+import System.Directory
+import System.Environment
+import System.Exit
+import Text.Printf
+import Text.Read
+
+import Lib.Interplay
+
+
+parseAudioVector :: String -> IO (Maybe AudioVector)
+parseAudioVector (readMaybe -> Just delay) = pure . Just . silence $ delay
+parseAudioVector filePath = do
+  fileExists <- doesFileExist filePath
+  if fileExists
+  then Just <$> load filePath
+  else pure $ Nothing
+
+
+printUsage :: IO ()
+printUsage = do
+  progName <- getProgName
+  putStrLn "usage:"
+  printf   "  %s [FILE | DELAY]\n" progName
+  putStrLn ""
+  putStrLn "Plays each of the audio FILEs one after the other."
+  putStrLn "DELAY pauses for the given number of seconds."
+  putStrLn "Use negative delays to overlay the end of one FILE on the beginning of the next!"
+
+interplay :: [String] -> IO ()
+interplay args = do
+  audioVectorMay <- mconcat <$> traverse parseAudioVector args
+  case audioVectorMay of
+    Nothing -> do
+      printUsage
+      exitFailure
+    Just audioVector -> do
+      play_ audioVector
