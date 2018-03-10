276f2fc091b89c40d20bed02bc6cf4f9b3210759
Samuel Gélineau <gelisam+github@gmail.com>
2018-03-10 10:18:07 -0500

    re-export a user-friendly API

--- git-slides/src/Lib/GitSlides.hs
+++ git-slides/src/Lib/GitSlides.hs
@@ -1,1 +1,10 @@
-module Lib.GitSlides where
+{-# LANGUAGE FlexibleContexts, LambdaCase, ScopedTypeVariables #-}
+module Lib.GitSlides
+  ( Slide
+  , currentSlide, prevSlide, nextSlide, lastSlide
+  , fileAtSlide
+  ) where
+
+import Lib.GitSlides.File
+import Lib.GitSlides.Navigation
+import Lib.GitSlides.Types
